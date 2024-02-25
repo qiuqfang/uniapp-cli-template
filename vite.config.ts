86cebe7fe7d56a1b6fc528a34694ab4c49222728
Quincy Qiu <qiuqfang@outlook.com>
2024-02-25 20:28:23 +0800

    chore: 添加 eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import UnoCSS from "unocss/vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni(), UnoCSS()],
+  plugins: [uni(), UnoCSS(), eslint()],
 });
